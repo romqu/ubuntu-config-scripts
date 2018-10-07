14f2e4333235af163d654b32b55f452991cd9766
romqu <roman.quistler@gmail.com>
2018-10-07 09:55:11 +0200

    pushed

--- installation/005-graphic-drivers.sh
+++ installation/005-graphic-drivers.sh
@@ -1,6 +1,5 @@
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update
-sudo apt-get dist-upgrade
 sudo apt-get --no-install-recommends install nvidia-390
 sudo apt-get install intel-microcode
 sudo apt-get --no-install-recommends install nvidia-settings
