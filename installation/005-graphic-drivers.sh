49326517b0d5a1643f63302a49bd2f5e26ce0683
romqu <roman.quistler@gmail.com>
2018-10-09 11:02:25 +0200

    pushed

--- installation/005-graphic-drivers.sh
+++ installation/005-graphic-drivers.sh
@@ -5,4 +5,9 @@ sudo apt-get install intel-microcode
 sudo apt-get --no-install-recommends install nvidia-settings
 sudo apt-get install prime-select
 
+
+sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo update-initramfs -u
+
 prime-select intel
